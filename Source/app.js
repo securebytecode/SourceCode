ee21977240c65e89c4fcc7dc3b40a34f00c39c31
securebytecode <139125231+securebytecode@users.noreply.github.com>
2023-07-26 21:22:16 +0530

    Add files via upload

---
+++ Source/app.js
@@ -0,0 +1,17 @@
+const boxes=document.querySelector('.grid-item');
+window.addEventListener('scroll' , checkBoxes);
+checkBoxes();
+
+function checkBoxes(){
+    const triggerButton=window.innerHeight /5 * 4;
+    boxes.forEach((box) => {
+        const boxTop= box.getBoundingClientRect().top;
+
+        if(boxTop < triggerButton){
+            box.classList.add('show');
+        }else{
+            box.classList.remove('show');
+        }
+        
+    });
+}
